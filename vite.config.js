62d172a4e2a55a94b49fbdc04f4a2588c33a46fe
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-10-04 15:33:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,57 +1,11 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import commonjs from '@rollup/plugin-commonjs';
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, 'env');
-  const htmlPlugin = () => {
-    return {
-      name: 'html-transform',
-      // Replace %VITE_X% env variables in index.html.
-      transformIndexHtml(html: string) {
-        return html.replace(/%(.*?)%/g, function match(_match, p1) {
-          return env[p1];
-        });
-      },
-    };
-  };
-
-  return {
-    resolve: {
-      alias: {
-        'react/jsx-runtime': 'react/jsx-runtime.js',
-      },
-    },
-    define: {
-      'process.env': process.env,
-    },
-    build: {
-      outDir: './build',
-      rollupOptions: {
-        plugins: [commonjs()],
-      },
-      commonjsOptions: {
-        exclude: [/./],
-      },
-    },
-    publicDir: './public',
-    plugins: [htmlPlugin(), react({
-      babel: {
-        plugins: [
-          ['@babel/plugin-proposal-decorators', { legacy: true }],
-          ['@babel/plugin-proposal-class-properties', { loose: false }],
-          'babel-plugin-transform-typescript-metadata',
-          'babel-plugin-parameter-decorator',
-          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
-        ]
-      },
-    })],
-    server: {
-      fs: {
-        // Allow serving files from workspace root, the fonts are for some reason fetched from there!
-        allow: ['../..'],
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [reactRefresh()],
+  mode: "development",
+  build: {
+    minify: false,
+  }
 });
